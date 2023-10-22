c2ec3471210289f9f459b7fb308c16d255d64e93
Neuromancer <79415352+Trawmoney@users.noreply.github.com>
2023-10-22 01:40:04 -0400

    Update route.ts

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -4,7 +4,7 @@ import { OpenAIStream, StreamingTextResponse } from 'ai';
 // Create an OpenAI API client (that's edge friendly!)
 // but configure it to point to fireworks.ai
 const fireworks = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY || '',
+  apiKey: 'ku9UYtzjSAATlcAstO8yrB89MzvDqJL3lGIkNgnVZ7URxPxK',
   baseURL: 'https://api.fireworks.ai/inference/v1',
 });
 // IMPORTANT! Set the runtime to edge
@@ -13,8 +13,8 @@ export async function POST(req: Request) {
   // Extract the `messages` from the body of the request
   const { messages } = await req.json();
 
-  // Ask Fireworks for a streaming chat completion using Llama 2 70b model
-  // @see https://app.fireworks.ai/models/fireworks/llama-v2-70b-chat
+  // Ask Fireworks for a streaming chat completion using Mistral 7b Instruct 4k
+  // @see https://app.fireworks.ai/models/fireworks/mistral-7b-instruct-4k
   const response = await fireworks.chat.completions.create({
     model: 'accounts/fireworks/models/mistral-7b-instruct-4k',
     stream: true,
